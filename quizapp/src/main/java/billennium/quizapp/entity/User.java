1e001a9bf70fa5ac3db8a3955a76ab50a7fe0057
Szymon <38376863+beatshaker@users.noreply.github.com>
2020-04-08 09:00:12 +0200

    add dev data base config

--- quizapp/src/main/java/billennium/quizapp/entity/User.java
+++ quizapp/src/main/java/billennium/quizapp/entity/User.java
@@ -1,18 +1,12 @@
 package billennium.quizapp.entity;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
+import javax.persistence.*;
 
-@Entity
+@Entity(name = "tuser")
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
